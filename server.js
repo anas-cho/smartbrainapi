ca6c9b2cb024f1dacb5cb77bc1609874da3c4163
anas-cho <a.chokairy@gmail.com>
2022-01-17 21:25:44 +0100

    fixed port

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.put('/image', image.handleImage(dbk))
 
 app.post('/imageurl', image.handleImageUrl)
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
